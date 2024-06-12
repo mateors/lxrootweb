7f4a978748fcfccad36e580a723b98a3f44749bb
Mostain Billah <bill.rassel@gmail.com>
2024-06-12 23:52:55 +0600

    code refactored

--- database.go
+++ database.go
@@ -938,7 +938,7 @@ func addressList(accountId string) []map[string]interface{} {
 func docNumberToAccountInfo(docNumber string) map[string]interface{} {
 
 	qs := `SELECT a.account_name,a.email,d.receipt_url,d.total_payable FROM lxroot._default.doc_keeper d LEFT JOIN lxroot._default.account a ON d.account_id=a.id WHERE d.doc_number=%q;`
-	sql := fmt.Sprintf(qs)
+	sql := fmt.Sprintf(qs, docNumber)
 	row, err := singleRow(sql)
 	if err != nil {
 		log.Println(err)
